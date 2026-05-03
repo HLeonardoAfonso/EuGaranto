dc7abeeb7c5e9892084563e00a36b5bc2bc5625d
Leonardo Afonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2026-05-03 19:27:24 +0100

    Added icons and a page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'new-product',
+    loadChildren: () => import('./new-product/new-product.module').then( m => m.NewProductPageModule)
   }
 ];
 @NgModule({
