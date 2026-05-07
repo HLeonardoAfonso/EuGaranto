8cda507f40cf91424a5a02f3291505201846a556
Leonardo Afonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2026-05-07 22:12:37 +0100

    Added product cards

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
-
+  public appPages = [
+    { nome: 'Aspirador' },
+    { nome: 'TV' },
+  ];
   constructor() {}
 
 }
+
+
